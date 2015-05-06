ecebca880a532a8087f59fd67960ad01e968aa25
Geno1024 <754097987@qq.com>
2015-05-06 17:46:02 +0800

    [init]

--- src/com/geno/dellineadder/MainActivity.java
+++ src/com/geno/dellineadder/MainActivity.java
@@ -4,14 +4,34 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.view.View.*;
 
 public class MainActivity extends Activity
 {
+	public Button b;
+	public EditText e;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		e=(EditText) findViewById(R.id.in);
+		b=(Button) findViewById(R.id.out);
+		b.setOnClickListener(new OnClickListener()
+			{
+				@Override
+				public void onClick(View p1)
+				{
+					String o="̶";
+					String s=e.getText().toString();
+					for(int i = 0;i < s.length();i++)
+					{
+						o=o+s.charAt(i)+"̶";
+					}
+					e.setText(o);
+				}
+			}
+		);
     }
 }
